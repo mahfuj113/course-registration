868c9d89194cfbf993c6173331f3ff56d4b39744
mahfuj113 <md.mahfujhasan894@gmail.com>
2023-09-15 02:29:26 +0600

    when button click show credit remaining,course name,total credit

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -4,12 +4,41 @@ import { FaDollarSign, FaBookOpen } from 'react-icons/fa';
 import Cart from "../Cart/Cart";
 
 const Cards = () => {
-    const [courses, setCourses] = useState([])
+    const [courses, setCourses] = useState([]);
+    const [selectedCourse, setSelectedCourse] = useState([]);
+    const [totalCredit, setTotalCredit] = useState(0);
+    const [creditRemaining, setCreditRemaininng] = useState(0);
+
+    const budgetCredit = 20;
+
     useEffect(() => {
         fetch("./data.json")
             .then(res => res.json())
             .then(data => setCourses(data))
     }, [])
+    const handleAddCourse = course => {
+        const isExist = selectedCourse.find(item => item.id === course.id)
+        let credit = course.credit
+        if (isExist) {
+            alert("Already booked try another")
+        }
+        else {
+
+            setSelectedCourse([...selectedCourse, course]);
+            selectedCourse.forEach(course => {
+                credit += course.credit;
+            });
+        }
+        const totalSum = budgetCredit - credit;
+        if(totalSum < 0){
+            return alert("No credit remaining")
+        }
+        else{
+            setTotalCredit(credit);
+            setCreditRemaininng(totalSum)
+        }
+        console.log(creditRemaining);
+    }
     return (
         <div className="bg-[#F3F3F3] flex gap-6 justify-center">
             <div className="grid grid-cols-3 gap-6">
@@ -31,7 +60,7 @@ const Cards = () => {
                                         <span className="text-base font-medium text-[#1C1B1B99]">Credit: {course.credit}hr</span>
                                     </div>
                                 </div>
-                                <div className="py-2 bg-[#2F80ED] rounded-lg text-center">
+                                <div onClick={() => handleAddCourse(course)} className="py-2 bg-[#2F80ED] rounded-lg text-center">
                                     <button className="text-lg font-semibold text-[#FFF] ">Select</button>
                                 </div>
                             </div>
@@ -40,7 +69,9 @@ const Cards = () => {
                 }
             </div>
             <div className="w-[312px]">
-                <Cart></Cart>
+                <Cart selectedCourse={selectedCourse}
+                    totalCredit={totalCredit}
+                    creditRemaining={creditRemaining}></Cart>
             </div>
         </div>
     );
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,20 +1,31 @@
 import PropTypes from 'prop-types'
 
-const Cart = props => {
+const Cart = ({selectedCourse,totalCredit, creditRemaining}) => {
+    console.log(creditRemaining);
   return (
     <div className='bg-[#FFF] rounded-xl'>
-        <h3 className='text-[#2F80ED] text-lg font-bold pt-6 mb-4 ml-6'>Credit Hour Remaining 7 hr</h3>
+        <h3 className='text-[#2F80ED] text-lg font-bold pt-6 mb-4 ml-6'>Credit Hour Remaining {creditRemaining} hr</h3>
         <div className='border bg-[#1C1B1B99] w-[264px] ml-6'></div>
         <h1 className='text-xl font-bold mt-4 mb-5 ml-6'>Course Name</h1>
-        <div className='ml-6'></div>
+        <div className='ml-6 mb-6'>
+            {
+                selectedCourse.map(course => (
+                    <li key={course.id} className='list-decimal text-base font-normal text-[#1C1B1B99]'>{course.course_name}</li>
+                ))
+            }
+        </div>
         <div className='border bg-[#1C1B1B99] w-[264px] ml-6'></div>
-        <p className='my-4 ml-6 text-base font-medium'>Total Credit Hour : 13</p>
+        <p className='my-4 ml-6 text-base font-medium'>Total Credit Hour : {totalCredit}</p>
         <div className='border bg-[#1C1B1B99] w-[264px] ml-6'></div>
-        <p className='mt-4 pb-6 ml-6'>Total Price : 48000 USD</p>
+        {/* <p className='mt-4 pb-6 ml-6'>Total Price : 48000 USD</p> */}
     </div>
   )
 }
 
-Cart.propTypes = {}
+Cart.propTypes = {
+    selectedCourse: PropTypes.array.isRequired,
+    totalCredit: PropTypes.number.isRequired,
+    creditRemaining: PropTypes.number.isRequired,
+}
 
 export default Cart
